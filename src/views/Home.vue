0f2028a28516906777421d81066cbf28e7f3b21c
Simon <yakunlang@163.com>
2021-02-08 15:15:04 +0800

    'Home页的制作'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,111 @@
 <template>
 <div>
-  Home
+  <el-container>
+    <el-header class="homeHeader">
+      <div class="title">微人事</div>
+      <el-dropdown class="userInfo" @command="commandHandler">
+  <span class="el-dropdown-link">
+    {{ user.name }}<i><img :src="user.userface" alt=""></i>
+  </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="userInfo">个人中心</el-dropdown-item>
+          <el-dropdown-item command="setting">设置</el-dropdown-item>
+          <el-dropdown-item command="logout" divided>注销登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-header>
+    <el-container>
+
+
+      <el-aside width="200px">
+        <el-menu router>
+          <el-submenu :index="1" v-for="(item,index) in this.$router.options.routes" v-if="!item.hidden" :key="index">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{item.name}}</span>
+            </template>
+            <el-menu-item :index="child.path" v-for="(child,indexj) in item.children" :key="indexj">
+              {{child.name}}
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-main>
+        <router-view/>
+      </el-main>
+
+    </el-container>
+  </el-container>
 </div>
 </template>
 
 <script>
 export default {
-name: "Home"
+name: "Home",
+  data() {
+    return {
+      user: JSON.parse(window.sessionStorage.getItem("user"))
+    }
+  },
+  methods: {
+    // menuClick(index) {
+    //   this.$router.push(index)
+    // },
+    commandHandler(cmd) {
+      if (cmd == 'logout') {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.getRequest("/logout");
+          window.sessionStorage.removeItem("user");
+          // this.$store.commit('initRoutes',[])
+          this.$router.replace("/")
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+.homeHeader {
+  background: #409eff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 15px;
+  box-sizing: border-box;
+
+}
+
+.homeHeader .title {
+  font-size: 30px;
+  font-family: 华文行楷;
+  color: #ffffff;
+}
+
+.homeHeader .userInfo {
+  cursor: pointer;
+}
+
+.el-dropdown-link img {
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  margin-left: 8px;
+}
+
+/*.el-dropdown-link {*/
+/*  display: flex;*/
+/*  align-items: center;*/
+/*}*/
 
 </style>
