f0b29e0f634ea8b19172b934c11708bc44931b8e
Simon <yakunlang@163.com>
2021-02-09 23:32:01 +0800

    '职位管理的前端页面设计'

--- src/components/sys/basic/PosMana.vue
+++ src/components/sys/basic/PosMana.vue
@@ -1,15 +1,62 @@
 <template>
 <div>
-  职位管理
+  <div>
+    <el-input
+        size="small"
+        class="addPosInput"
+        placeholder="添加职位..."
+        prefix-icon="el-icon-plus"
+        v-model="pos.name">
+    </el-input>
+    <el-button icon="el-icon-plus" size="small" type="primary">添加</el-button>
+  </div>
+
+  <div class="posManaMain">
+    <el-table
+        :data="positions"
+        border
+        stripe
+        size="small"
+        style="width: 70%">
+      <el-table-column
+          prop="id"
+          label="编号"
+          width="55">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="职务名称"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="createDate"
+          label="创建时间">
+      </el-table-column>
+    </el-table>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: "PosMana"
+  name: "PosMana",
+  data(){
+    return{
+      pos:{
+        name:''
+      },
+        positions: []
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+.addPosInput{
+  width: 300px;
+  margin-right: 8px;
+}
+.posManaMain{
+  margin-top: 10px;
+}
 </style>
