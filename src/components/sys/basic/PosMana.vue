2d1223976a4a1777799d4b41930186f39360886b
Simon <yakunlang@163.com>
2021-02-10 17:41:19 +0800

    '工作职位修改'

--- src/components/sys/basic/PosMana.vue
+++ src/components/sys/basic/PosMana.vue
@@ -1,98 +1,128 @@
 <template>
-<div>
   <div>
-    <el-input
-        size="small"
-        class="addPosInput"
-        placeholder="添加职位..."
-        prefix-icon="el-icon-plus"
-        @keydown.enter.native="addPosition"
-        v-model="pos.name">
-    </el-input>
-    <el-button icon="el-icon-plus" size="small" type="primary" @click="addPosition">添加</el-button>
-  </div>
+    <div>
+      <el-input
+          size="small"
+          class="addPosInput"
+          placeholder="添加职位..."
+          prefix-icon="el-icon-plus"
+          @keydown.enter.native="addPosition"
+          v-model="pos.name">
+      </el-input>
+      <el-button icon="el-icon-plus" size="small" type="primary" @click="addPosition">添加</el-button>
+    </div>
 
-  <div class="posManaMain">
-    <el-table
-        :data="positions"
-        border
-        stripe
-        size="small"
-        style="width: 70%">
-      <el-table-column
-          type="selection"
-          width="55">
-      </el-table-column>
-      <el-table-column
-          prop="id"
-          label="编号"
-          width="55">
-      </el-table-column>
-      <el-table-column
-          prop="name"
-          label="职务名称"
-          width="120">
-      </el-table-column>
-      <el-table-column
-          prop="createDate"
-          label="创建时间">
-      </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-          <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class="posManaMain">
+      <el-table
+          :data="positions"
+          border
+          stripe
+          size="small"
+          style="width: 70%">
+        <el-table-column
+            type="selection"
+            width="55">
+        </el-table-column>
+        <el-table-column
+            prop="id"
+            label="编号"
+            width="55">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="职务名称"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="createDate"
+            label="创建时间"
+            width="100">
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+                size="mini"
+                @click="showEditView(scope.$index, scope.row)">编辑
+            </el-button>
+            <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <el-dialog
+        title="修改职位"
+        :visible.sync="dialogVisible"
+        width="30%">
+      <div>
+        <el-tag>职位名称</el-tag>
+        <el-input class="updatePosInput" size="small" v-model="updatePos.name"></el-input>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false" size="small">取 消</el-button>
+    <el-button type="primary" @click="doUpdate" size="small">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
-</div>
 </template>
 
 <script>
 export default {
   name: "PosMana",
-  data(){
-    return{
-      pos:{
-        name:''
+  data() {
+    return {
+      pos: {
+        name: ''
       },
-        positions: []
+      updatePos: {
+        name: ''
+      },
+      dialogVisible: false,
+      positions: []
     }
   },
   mounted() {
     this.initPositions();
   },
-  methods:{
-    addPosition(){
-      if(this.pos.name){
-        this.postRequest("/system/basic/pos/",this.pos).then(resp=>{
-          if(resp){
+  methods: {
+    addPosition() {
+      if (this.pos.name) {
+        this.postRequest("/system/basic/pos/", this.pos).then(resp => {
+          if (resp) {
             this.initPositions();
-            this.pos.name='';
+            this.pos.name = '';
           }
         })
-      }else{
+      } else {
         this.$message.error("职位名称不可以为空")
       }
     },
-    handleEdit(index,data){
-
+    showEditView(index, data) {
+      Object.assign(this.updatePos, data)
+      this.dialogVisible = true;
     },
-    handleDelete(index,data){
+    doUpdate() {
+      this.putRequest("/system/basic/pos/", this.updatePos).then(resp => {
+        if (resp) {
+          this.initPositions();
+          this.updatePos.name = '';
+          this.dialogVisible = false;
+        }
+      })
+    },
+    handleDelete(index, data) {
       // 如果员工表内使用到了职位，则是删除不了的
-      this.$confirm('此操作将永久删除【'+data.name+'】, 是否继续?', '提示', {
+      this.$confirm('此操作将永久删除【' + data.name + '】, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-       this.deleteRequest("/system/basic/pos/"+data.id).then(resp=>{
-         this.initPositions();
-       })
+        this.deleteRequest("/system/basic/pos/" + data.id).then(resp => {
+          this.initPositions();
+        })
       }).catch(() => {
         this.$message({
           type: 'info',
@@ -100,10 +130,10 @@ export default {
         });
       });
     },
-    initPositions(){
-      this.getRequest("/system/basic/pos/").then(resp=>{
-        if(resp){
-          this.positions=resp;
+    initPositions() {
+      this.getRequest("/system/basic/pos/").then(resp => {
+        if (resp) {
+          this.positions = resp;
         }
       })
     }
@@ -112,11 +142,17 @@ export default {
 </script>
 
 <style>
-.addPosInput{
+.addPosInput {
   width: 300px;
   margin-right: 8px;
 }
-.posManaMain{
+
+.updatePosInput {
+  width: 200px;
+  margin: 8px;
+}
+
+.posManaMain {
   margin-top: 10px;
 }
 </style>
