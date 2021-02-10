0a70e5f1be8436bbcfb085ecfe8f8f3c0ba4e5cc
Simon <yakunlang@163.com>
2021-02-10 16:35:50 +0800

    '职位管理的前后端交互'

--- src/components/sys/basic/PosMana.vue
+++ src/components/sys/basic/PosMana.vue
@@ -6,9 +6,10 @@
         class="addPosInput"
         placeholder="添加职位..."
         prefix-icon="el-icon-plus"
+        @keydown.enter.native="addPosition"
         v-model="pos.name">
     </el-input>
-    <el-button icon="el-icon-plus" size="small" type="primary">添加</el-button>
+    <el-button icon="el-icon-plus" size="small" type="primary" @click="addPosition">添加</el-button>
   </div>
 
   <div class="posManaMain">
@@ -18,6 +19,10 @@
         stripe
         size="small"
         style="width: 70%">
+      <el-table-column
+          type="selection"
+          width="55">
+      </el-table-column>
       <el-table-column
           prop="id"
           label="编号"
@@ -31,6 +36,17 @@
       <el-table-column
           prop="createDate"
           label="创建时间">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
       </el-table-column>
     </el-table>
   </div>
@@ -46,6 +62,50 @@ export default {
         name:''
       },
         positions: []
+    }
+  },
+  mounted() {
+    this.initPositions();
+  },
+  methods:{
+    addPosition(){
+      if(this.pos.name){
+        this.postRequest("/system/basic/pos/",this.pos).then(resp=>{
+          if(resp){
+            this.initPositions();
+            this.pos.name='';
+          }
+        })
+      }else{
+        this.$message.error("职位名称不可以为空")
+      }
+    },
+    handleEdit(index,data){
+
+    },
+    handleDelete(index,data){
+      // 如果员工表内使用到了职位，则是删除不了的
+      this.$confirm('此操作将永久删除【'+data.name+'】, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+       this.deleteRequest("/system/basic/pos/"+data.id).then(resp=>{
+         this.initPositions();
+       })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    initPositions(){
+      this.getRequest("/system/basic/pos/").then(resp=>{
+        if(resp){
+          this.positions=resp;
+        }
+      })
     }
   }
 }
